1c59279966765cbe3d18244fcc48d2bd79b5d873
Henrik Trandem <HenrikT@users.noreply.github.com>
2025-04-30 12:03:48 +0200

    feat: design draft login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,30 @@
+import styles from "./page.module.css";
+import { FaUser, FaLock } from "react-icons/fa";
+
+export default function Login() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.card}>
+        <h1 className={styles.header}>Login</h1>
+        <form className={styles.form}>
+          <div className={styles.inputGroup}>
+            <FaUser className={styles.icon} />
+            <input
+              type="email"
+              name="email"
+              placeholder="Type your username"
+              className={styles.input}
+            />
+          </div>
+          <div className={styles.inputGroup}>
+            <FaLock className={styles.icon} />
+            <input type="password" placeholder="Type your password" className={styles.input} />
+          </div>
+          <button type="submit" className={styles.loginButton}>
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
