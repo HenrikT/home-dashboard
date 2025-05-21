04b29aa27cc89d7d5919502339543d73e8814d09
Henrik Trandem <HenrikT@users.noreply.github.com>
2025-05-22 01:22:20 +0200

    chore: make login a controlled component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,6 +7,9 @@ import { FaUser, FaLock } from "react-icons/fa";
 export default function Login() {
   const router = useRouter();
 
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     router.push("/home");
@@ -24,11 +27,20 @@ export default function Login() {
               name="email"
               placeholder="Type your username"
               className={styles.input}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className={styles.inputGroup}>
             <FaLock className={styles.icon} />
-            <input type="password" placeholder="Type your password" className={styles.input} />
+            <input
+              type="password"
+              name="password"
+              placeholder="Type your password"
+              className={styles.input}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <button aria-label="Login" type="submit" className={styles.loginButton}>
             Login
