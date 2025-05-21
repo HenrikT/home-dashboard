845f56dcedce0904a0c16b9db6991e28dce42507
Henrik Trandem <HenrikT@users.noreply.github.com>
2025-05-21 09:55:04 +0200

    chore: cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Login from "./login/page";
 
 export default function Home() {
@@ -8,12 +6,6 @@ export default function Home() {
       <main>
         <Login />
       </main>
-      <footer className={styles.footer}>
-        <a href="https://sørlys.no" target="_blank" rel="noopener noreferrer">
-          <Image aria-hidden src="/globe.svg" alt="File icon" width={16} height={16} />
-          Sørlys
-        </a>
-      </footer>
     </div>
   );
 }
