4d80ebe57ece669af6945f83f0ff4b51cefb2a73
Henrik Trandem <HenrikT@users.noreply.github.com>
2025-05-22 23:22:57 +0200

    test: fix supabaseUrl is required by using dotenv

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom";
+
+// Added as fix to: https://stackoverflow.com/questions/68239168/error-supabaseurl-is-required-supabase-postgresql
+import dotenv from "dotenv";
+dotenv.config({
+  path: ".env.local",
+});
